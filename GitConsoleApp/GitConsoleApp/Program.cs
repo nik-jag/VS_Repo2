71892aebfb1e34af954b14a2c6b9a63dc0180c13
nik-jag <nj.android.86@gmail,com>
2019-07-19 15:02:16 +0530

    1st commit from VS

---
+++ GitConsoleApp/GitConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GitHub!");
+        }
+    }
+}
